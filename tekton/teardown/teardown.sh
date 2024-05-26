298191b9128712eba3c90d4b0625fb9adec19508
Stephen W. Boyd <sboyd@electricaltrainingalliance.org>
2024-05-26 16:43:32 -0400

    fixes plus tekton upgrades

--- tekton/teardown/teardown.sh
+++ tekton/teardown/teardown.sh
@@ -2,6 +2,6 @@
 
 oc delete rolebinding case03752258-pipeline-sa-scc-rolebinding
 oc delete clusterrolebinding case03752258-pipelines-as-code-cleanup-job
-oc delete clusterrole pipelines-as-code-cleanup-job
+# oc delete clusterrole pipelines-as-code-cleanup-job
 oc delete sa case03752258-pipeline-sa
 oc delete project case03752258
